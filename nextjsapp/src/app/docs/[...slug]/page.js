447d82bb68ae1ecbbcd8ea89a2cfbf6213f069df
Pankaj Gusain <gusainpankaj1999@gmail.com>
2023-12-17 11:07:33 -0800

    create dynamic n level nested routes for /docs/feature1/example1/..  using  /docs/[...slug] catchAllSegment

---
+++ nextjsapp/src/app/docs/[...slug]/page.js
@@ -0,0 +1,9 @@
+export default function Docs(props){
+    if(props.params.slug.length == 2){
+        return <h1>Viewing docs for feature {props.params.slug[0]} and concept {props.params.slug[0]}  </h1>
+    }
+    else if(props.params.slug.length == 1){
+        return <h1>Viewing docs for feature {props.params.slug[0]}</h1>
+    }
+    return <h1>Docs Home Page</h1>
+}
