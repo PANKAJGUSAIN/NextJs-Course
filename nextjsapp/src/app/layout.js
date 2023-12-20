6de1dbfd3cc734ea98c4e5b0deeb9ba843f5c078
Pankaj Gusain <gusainpankaj1999@gmail.com>
2023-12-20 03:29:15 -0800

    create root layout header and footer

--- nextjsapp/src/app/layout.js
+++ nextjsapp/src/app/layout.js
@@ -6,7 +6,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
  return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+      <header style={{backgroundColor:'lightblue' , padding:'1rem'}}>
+        <p>Header</p>
+      </header>
+      {children}
+      <footer style={{backgroundColor:'ghostwhite' , padding:'1rem'}}>
+        <p>Footer</p>
+      </footer>
+      </body>
     </html>
   )
 }
