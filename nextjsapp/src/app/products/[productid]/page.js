15ee1bca11c88534c3e2578f8cc5e32b87dfef93
Pankaj Gusain <gusainpankaj1999@gmail.com>
2023-12-27 08:31:03 -0800

    created dynamic metadata using generateMetadata function for /[productid] route segment

--- nextjsapp/src/app/products/[productid]/page.js
+++ nextjsapp/src/app/products/[productid]/page.js
@@ -1,3 +1,18 @@
+
+export const generateMetadata = async ({params}) => {
+    const desc = await new Promise (resolve =>{
+        setTimeout(() => {
+            resolve(
+                `iphone ${params.productid}`
+            )
+        }, 100);
+    })
+    return {
+        title:`Product ${params.productid}`,
+        description : desc
+    }
+}
+
 export default function productdetails(props){
     // console.log not displaying on the developers tool why? but id is showing fine on the ui
     // console.log('fetching product id from route params' , props)
