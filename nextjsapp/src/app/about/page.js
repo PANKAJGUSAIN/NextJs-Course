00fc8cd917a4aca5a4ee41ea11a8941518505ea8
Pankaj Gusain <gusainpankaj1999@gmail.com>
2023-12-27 07:55:36 -0800

    created static meta data for about page

--- nextjsapp/src/app/about/page.js
+++ nextjsapp/src/app/about/page.js
@@ -1,3 +1,8 @@
+export const metadata = {
+    title:'About Section',
+    description:'this this description for the about section'
+}
+
 export default function About(){
     return (
         <h1>
