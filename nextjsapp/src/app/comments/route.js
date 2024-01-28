515ad8135ec9ee360a884545bf800e602fce5574
Pankaj Gusain <gusainpankaj1999@gmail.com>
2024-01-28 04:31:12 -0800

    handling POST REQUEST /comments

--- nextjsapp/src/app/comments/route.js
+++ nextjsapp/src/app/comments/route.js
@@ -3,3 +3,20 @@ import {comments} from './data'
 export async function GET(){
     return Response.json(comments)
 }
+
+export async function POST(request){
+   const comment = await request.json()  // gets the new comment  from the request 
+   const newComment = {                 // provides the new comment with id 
+    id : comments.length + 1 , 
+    text: comment.text
+   }
+
+   comments.push(newComment)                // pushes the new comment to the 
+    return new Response(JSON.stringify(newComment) , {
+        headers :{ 
+            "Content-Type" :"application/json"
+        } , 
+        status : 201
+    })
+
+}
