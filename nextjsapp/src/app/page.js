d7f501b8b02a8a0d184b230592f9766f88971bdb
Pankaj Gusain <gusainpankaj1999@gmail.com>
2024-01-28 03:41:29 -0800

    Case : when page.js and route.js is placed in same place (router.js will run ) /users

--- nextjsapp/src/app/page.js
+++ nextjsapp/src/app/page.js
@@ -4,7 +4,9 @@ export default function Home (){
     return (
         <>
         <h1>HOME PAGE</h1>
-        <Link  href={'/Dashboard'} > Dashboard get  Api route </Link>
+        <Link  href={'/Dashboard'} > Dashboard GET  Api route </Link>
+        <br/>
+        <Link  href={'/users'} > Case : when page.js and route.js is placed in same place (router.js will run ) </Link>
         </>
     )
 }
