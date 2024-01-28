1c75f81d476450eee3fa679ad6d6d1621574a2dd
Pankaj Gusain <gusainpankaj1999@gmail.com>
2024-01-28 01:42:41 -0800

    created GET Route handler for /Dashboard

--- nextjsapp/src/app/page.js
+++ nextjsapp/src/app/page.js
@@ -1,8 +1,10 @@
+import Link from "next/link";
 
 export default function Home (){
     return (
         <>
         <h1>HOME PAGE</h1>
+        <Link  href={'/Dashboard'} > Dashboard get  Api route </Link>
         </>
     )
 }
