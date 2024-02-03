ec6fb552c71d847f91d2c100b679a55a094b0d3c
Pankaj Gusain <gusainpankaj1999@gmail.com>
2024-02-03 06:09:23 -0800

    learned URL query parameter in routeHandlers  added comments filtering for /comments?query=value

--- nextjsapp/src/app/comments/route.js
+++ nextjsapp/src/app/comments/route.js
@@ -1,7 +1,10 @@
 import {comments} from './data'
 
-export async function GET(){
-    return Response.json(comments)
+export async function GET(request){
+    const searchParams = request.nextUrl.searchParams
+    const query = searchParams.get('query')
+    const filteredComments = query ? comments.filter(comment => comment.text.includes(query)):comments
+    return Response.json(filteredComments)
     //OR
     // return new Response(JSON.stringify(comments))
 }
