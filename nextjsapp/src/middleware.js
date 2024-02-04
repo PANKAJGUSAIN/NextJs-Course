4d70d2fcbaac63d6e25c479e2c2ec70f797d2c25
Pankaj Gusain <gusainpankaj1999@gmail.com>
2024-02-04 02:08:35 -0800

    way2 - creating middleware to redirect to / if trying to react /profile using conditional statement

--- nextjsapp/src/middleware.js
+++ nextjsapp/src/middleware.js
@@ -2,11 +2,21 @@ import { NextResponse } from "next/server"; // extends the standard Response web
 
 
 // way 1   this middleware() fn will only work if it maches the url defined in config
+// export function middleware(request){
+//     return NextResponse.redirect(new URL('/' , request.url))
+// }
+
+// export const config = {
+
+//     matcher :'/profile'
+// }
+
+
+
+// way2 using condtional statement  this middleware() fn will be called everytime to check the condition
 export function middleware(request){
-    return NextResponse.redirect(new URL('/' , request.url))
-}
-
-export const config = {
-
-    matcher :'/profile'
+    if(request.nextUrl.pathname === '/profile'){
+        return NextResponse.redirect(new URL('/' , request.url))
+    }
+    
 }
