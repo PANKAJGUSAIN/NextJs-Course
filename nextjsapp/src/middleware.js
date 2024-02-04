fe92177e8057c3286911acc02745218f492cb99c
Pankaj Gusain <gusainpankaj1999@gmail.com>
2024-02-04 02:18:53 -0800

    rewrite in middleware

--- nextjsapp/src/middleware.js
+++ nextjsapp/src/middleware.js
@@ -16,7 +16,8 @@ import { NextResponse } from "next/server"; // extends the standard Response web
 // way2 using condtional statement  this middleware() fn will be called everytime to check the condition
 export function middleware(request){
     if(request.nextUrl.pathname === '/profile'){
-        return NextResponse.redirect(new URL('/' , request.url))
+        // return NextResponse.redirect(new URL('/' , request.url))
+        return NextResponse.rewrite(new URL('/' , request.url)) // rewrite means the url will remain same ie '/profile' but the content will be of '/'
     }
     
 }
